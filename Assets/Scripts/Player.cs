4fd4c03b48fec5242502bb93f512ebce86b13541
Daniel Lerche <daniel.lerche10@gmail.com>
2022-03-21 17:43:10 +0100

    Added movable cube over network

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,21 @@
+using Mirror;
+using UnityEngine;
+public class Player : NetworkBehaviour
+{
+    
+    void HandleMovement() 
+    {
+        if(isLocalPlayer)
+        {
+            float moveHorizontal = Input.GetAxis("Horizontal");
+            float moveVertical = Input.GetAxis("Vertical");
+            Vector3 movement = new Vector3(moveHorizontal * 0.3f,  moveVertical * 0.3f, 0);
+            transform.position = transform.position + movement;
+        }
+    }
+
+    void Update() 
+    {
+        HandleMovement();
+    }
+}
